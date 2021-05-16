1de99b54eed7903ba399333602d9f52a120ab3cb
Thomas Pelletier <thomas@pelletier.codes>
2021-05-16 08:04:07 -0400

    Reuse existing map

---
+++ types.go
@@ -0,0 +1,11 @@
+package toml
+
+import (
+	"encoding"
+	"reflect"
+	"time"
+)
+
+var timeType = reflect.TypeOf(time.Time{})
+var textMarshalerType = reflect.TypeOf(new(encoding.TextMarshaler)).Elem()
+var mapStringInterfaceType = reflect.TypeOf(map[string]interface{}{})
