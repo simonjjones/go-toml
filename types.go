b3287b3de14339881f3aac182b4c94defd7036c0
Thomas Pelletier <thomas@pelletier.codes>
2021-05-18 09:45:40 -0400

    wip

--- types.go
+++ types.go
@@ -9,3 +9,4 @@ import (
 var timeType = reflect.TypeOf(time.Time{})
 var textMarshalerType = reflect.TypeOf(new(encoding.TextMarshaler)).Elem()
 var mapStringInterfaceType = reflect.TypeOf(map[string]interface{}{})
+var sliceInterfaceType = reflect.TypeOf([]interface{}{})
